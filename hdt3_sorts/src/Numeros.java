b74700e212fbca4f53e7163498a60bfa017ad604
arriaza96 <arriaza96@172.20.13.148>
2016-07-27 11:35:09 -0600

    v1

---
+++ hdt3_sorts/src/Numeros.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rodrigo Arriaza;
+ */
+import java.util.ArrayList;
+import java.util.Random;
+public class Numeros {
+    
+        
+public static int[] generarNumeros(){
+        int losnumeros[] = new int[3000];
+        for (int i=0; i<=3000; i++)
+        {      
+            Random rnd = new Random();
+            losnumeros[i] = (int) (rnd.nextDouble() * 100);
+        }
+    return losnumeros;
+    } 
+}
